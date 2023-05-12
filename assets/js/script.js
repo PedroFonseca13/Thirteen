ecbb8d5d3421553a8f81eb0e06e7fa2b3c991c64
Pedro Fonseca <pedrinhopedro01@gmail.com>
2023-05-12 15:32:05 -0300

    feat: pagina estilizada para telas ate 576px

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,30 @@
-var services = document.querySelector('.services-title')
-
-function handleIntersection(entries, observer) {
-	entries.forEach(function (entry) {
-		if (entry.isIntersecting) {
-			// O elemento está visível na tela
-			console.log('O elemento está sendo exibido')
-			services.classList.add('animate__animated', 'animate__fadeInLeft')
-		} else {
-			// O elemento não está visível na tela
-			console.log('O elemento não está sendo exibido')
-		}
-	})
-}
-
-// Seleciona o elemento que deseja observar
-var meuElemento = document.querySelector('.services-title')
-
-// Cria uma nova instância do Intersection Observer
-var observer = new IntersectionObserver(handleIntersection)
-
-// Inicia a observação do elemento
-observer.observe(meuElemento)
-
 // função AOS
 AOS.init({
 	duration: 900,
 	easing: 'easeOut',
 	once: true,
 })
+
+// Obtém os elementos do menu
+const menuHamburguer = document.querySelector('.mobile-menu')
+const menuItems = document.querySelector('.nav-list')
+const menuLinks = document.querySelectorAll('.nav-list li')
+console.log(menuLinks)
+
+// Adiciona um evento de clique no menu hamburguer
+menuHamburguer.addEventListener('click', () => {
+	// Alternar a classe 'open' para mostrar/ocultar os itens do menu
+	menuItems.classList.toggle('active')
+	menuHamburguer.classList.toggle('active')
+	animateLinks(menuLinks)
+})
+
+function animateLinks(element) {
+	element.forEach((link, index) => {
+		link.style.animation
+			? (link.style.animation = '')
+			: (link.style.animation = `navLinkFade 0.5s ease forwards ${
+					index / 7 + 0.3
+			  }s`)
+	})
+}
